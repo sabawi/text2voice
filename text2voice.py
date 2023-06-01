184b65be76868de0e15f97ad791c016ac92dd153
Al Sabawi <sabawi@gmail.com>
2023-06-01 08:18:56 -0400

    code restructuring

--- text2voice.py
+++ text2voice.py
@@ -1,51 +1,66 @@
-# python 3+ only
-import os
-import platform
-
 import pyttsx3
 from gtts import gTTS
+import sys
+import os
 
-PLATFORM = platform.system().lower()
-OS_RELEASE = platform.release()
-
-# Setup pytts voice engine
-pytts = pyttsx3.init()
-pyttsVolume = pytts.getProperty('volume')
-pytts.setProperty('volume', 1.25)
-pyttsRate = pytts.getProperty('rate')
-pytts.setProperty('rate', 150)
-pytts.setProperty('voice', 'english')
-pyttsVoices = pytts.getProperty('voices')
-
-pytts.setProperty('voice', pyttsVoices[1])
-PROMPT = 'Type something>>'
-
-
-def linuxVoices(line):
-    # Uses google's gtts package
+def linux_voices(line):
+    # Uses Google's gTTS package
     tts = gTTS(text=line, lang='en')
     tts.save('line.mp3')
     os.system("mpg321 -q line.mp3")
     os.remove('line.mp3')
 
-
-def windowsVoices(line):
+def windows_voices(line):
     # Uses pyttsx3 package
     pytts.say(line)
     pytts.runAndWait()
 
+def get_char():
+    """Get a single character from standard input"""
+    import tty, termios
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    try:
+        tty.setraw(sys.stdin.fileno())
+        ch = sys.stdin.read(1)
+    finally:
+        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+    return ch
 
 def main():
-    ptatform2voice = \
-        {
-            'linux': linuxVoices,
-            'windows': windowsVoices
-        }
+    pytts = pyttsx3.init()
+    pytts.setProperty('volume', 1.25)
+    pytts.setProperty('rate', 150)
+    pytts.setProperty('voice', 'english')
+    pytts_voices = pytts.getProperty('voices')
+    pytts.setProperty('voice', pytts_voices[1])
 
-    inp = input(PROMPT)
-    while (not inp == '!q'):
-        ptatform2voice[PLATFORM](inp)
-        inp = input(PROMPT)
+    platform2voice = {
+        'linux': linux_voices,
+        'windows': windows_voices
+    }
+
+    prompt = 'Type something: '
+
+    print(prompt, end='', flush=True)
+    while True:
+        line = ""
+        while True:
+            ch = get_char()
+            line += ch
+            print(ch, end='', flush=True)
+            if ch == '\n' or ch == '\r': 
+                print("\n")
+                break
+        
+        if line.lower().strip() in ("-q", "-quit"):
+            print("Bye!")
+            break
+        elif len(line.strip()) > 0:
+            platform2voice[sys.platform](line)
+            print(prompt, end='', flush=True)
+
+if __name__ == '__main__':
+    main()
 
 
-main()
